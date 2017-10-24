2ba5837a86dd997b7a5b32982e33e9fda6aa44aa
MY RAILS <jhambrohan44@gmail.com>
2017-10-25 03:37:27 +0530

    modified database.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015182310) do
+ActiveRecord::Schema.define(version: 20171024212025) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.decimal "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
