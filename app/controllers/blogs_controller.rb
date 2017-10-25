c2775dd5b2cacfdd14783c80cf42c86e29f42111
MY RAILS <jhambrohan44@gmail.com>
2017-10-26 02:42:05 +0530

    implemented globbing for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -71,6 +71,10 @@ class BlogsController < ApplicationController
     end
   end
 
+  def missing
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'blogs/*missing' , to: 'blogs#missing'
 end
