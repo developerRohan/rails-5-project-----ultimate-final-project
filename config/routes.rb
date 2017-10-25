427e15babf281f3c7233d9f3a888fa3bdff843a4
MY RAILS <jhambrohan44@gmail.com>
2017-10-25 20:21:46 +0530

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # this is restful routing
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
