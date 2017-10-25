25ada20153052f2d03ad9238253e4752579fd65d
MY RAILS <jhambrohan44@gmail.com>
2017-10-25 20:19:54 +0530

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
+  
 
-  get 'pages/about'
-
-  get 'pages/contact'
+  root to: 'pages#home'
+  get 'itsme-rohan', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   # this is restful routing
   resources :blogs
+  resources :portfolios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
