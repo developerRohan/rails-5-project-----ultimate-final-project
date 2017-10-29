54bee855850fcb98d46aeb9d93d8372074b1bea3
MY RAILS <jhambrohan44@gmail.com>
2017-10-29 17:30:02 +0530

    added and used  my own gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
 		content_tag(:p ,greeting , class: "source-greeting" )
   	end
   end
+
+  def copyright_generator
+    CopyrightGem::Renderer.copyright "rohan arora" , "ruby on rails developer"
+  end
 end
