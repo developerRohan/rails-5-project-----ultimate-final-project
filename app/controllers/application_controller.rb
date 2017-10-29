72edec091c774218ca41bb285b6014a15595d15b
MY RAILS <jhambrohan44@gmail.com>
2017-10-29 16:57:20 +0530

    stratigise copyright gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,21 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
 
-  before_action :set_title
+  before_action :set_title , :set_copyright
 	def set_title
     	@page_title = "itsme-ROHAN | My Portfolio Website"
   	end
 
- 
+  	def set_copyright
+  		@copyright = CopyrightGem::Renderer.copyright "rohan arora" , "ruby on rails developer"
+  	end
 
+
+module CopyrightGem
+	class Renderer
+		def self.copyright name , designation
+			"&copy; | #{name} - #{designation} | All rights reserved".html_safe
+		end
+	end
+end
 end
