48aa1e73cf9d64cc4cf3125ce0dbf6c7f68ad848
MY RAILS <jhambrohan44@gmail.com>
2017-11-01 22:40:46 +0530

    added position attribute in portfolios table to implement drag and drop

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,10 +5,15 @@ class Portfolio < ApplicationRecord
 	include Placeholder
 
 	validates_presence_of :title, :body, :main_image, :thumb_image
+	def self.by_position
+	  order("position ASC")
+	end
 	after_initialize :set_defaults
 
 	  def set_defaults
 	    self.main_image ||= Placeholder.image_generator(height: '600' , width: '400')
 	    self.thumb_image ||= Placeholder.image_generator(height: '350' , width: '200')
 	  end
+
+
 end
