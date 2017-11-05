a03601430fa3a813872a32678275e449500b5593
MY RAILS <jhambrohan44@gmail.com>
2017-11-05 16:12:13 +0530

    Created a comment broadcast, configured the websocket route and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'blogs/*missing' , to: 'blogs#missing'
+
+  mount ActionCable.server => '/cable'
+  
 end
