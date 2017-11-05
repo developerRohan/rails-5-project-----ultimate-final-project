ed9e7e8f1c48c84b103223db1a03e7302d8907ed
MY RAILS <jhambrohan44@gmail.com>
2017-11-05 17:43:40 +0530

    implemented actioncable for comments

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,34 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+  	identified_by :current_user
+
+  	def guest_user
+	  guest = GuestUser.new
+	  guest.id = guest.object_id
+	  guest.name = "Guest User"
+	  guest.first_name = "Guest"
+	  guest.last_name = "User"
+	  guest.email = "guest@user.com"
+	  guest
+	end
+
+    def connect
+      self.current_user = find_verified_user || guest_user
+  	  logger.add_tags 'ActionCable', current_user.email
+  	  logger.add_tags 'ActionCable', current_user.id
+    end
+
+
+
+    protected
+
+    def find_verified_user # this checks whether a user is authenticated with devise
+      if verified_user = env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+  end
   end
 end
