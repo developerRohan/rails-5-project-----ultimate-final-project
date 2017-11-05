8b1ed93011dd5a4eda37c3c42dc1185f0e2a4602
MY RAILS <jhambrohan44@gmail.com>
2017-11-05 18:03:32 +0530

    correct the code and implemented action cable completely

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+   mount ActionCable.server => '/cable'
   resources :comments
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   root to: 'pages#home'
@@ -19,6 +20,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'blogs/*missing' , to: 'blogs#missing'
 
-  mount ActionCable.server => '/cable'
+ 
   
 end
