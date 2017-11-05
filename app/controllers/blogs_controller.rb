5077c1ebd3194d4673aed1f610bc0a944a80a53e
MY RAILS <jhambrohan44@gmail.com>
2017-11-06 01:35:54 +0530

    aded topic view

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :authenticate_user!
+  
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blogs"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit , :toggle_status]}, site_admin: :all
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,13 @@ class TopicsController < ApplicationController
   	@topics = Topic.all
   end
 
-  def show
-  	@topic = Topic.find(params[:id])
+ def show
+    @topic = Topic.find(params[:id])
+
+    if logged_in?(:site_admin)
+      @blogs = @topic.blogs.recent.page(params[:page]).per(5)
+    else
+      @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
+    end
   end
 end
